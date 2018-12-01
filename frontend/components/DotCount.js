206f33a16fcc6568cae8557f69efc86af1bba5e5
Rick Woltheus <rwoltheus@gmail.com>
2018-12-01 15:18:09 +0100

    feat(*): add cart item remove

--- frontend/components/DotCount.js
+++ frontend/components/DotCount.js
@@ -1,6 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
+
+const AnimationStyles = styled.span`
+    position: relative;
+    .count {
+        display: block;
+        position: relative;
+        transition: all 0.4s;
+        backface-visibility: hidden;
+    }
+    /* initial-state */
+    .count-enter {
+        transform: scale(4) rotateX(0.5turn);
+    }
+    .count-enter-active {
+        transform: rotateX(0);
+    }
+    .count-exit {
+        top: 0;
+        position: absolute;
+        transform: rotateX(0);
+    }
+    .count-exit-active {
+        transform: scale(4) rotateX(0.5turn);
+    }
+`
 const Dot = styled.div`
     background: ${props => props.theme.red};
     color: white;
@@ -15,9 +41,21 @@ const Dot = styled.div`
 `
 
 const CartCount = ({ count }) => (
-    <Dot>
-        {count}
-    </Dot>
+    <AnimationStyles>
+        <TransitionGroup>
+            <CSSTransition
+                unmountOnExit
+                className="count"
+                classNames="count"
+                key={count}
+                timeout={{ enter: 400, exit: 400}}
+            >
+                <Dot>
+                    {count}
+                </Dot>
+            </CSSTransition>
+        </TransitionGroup>
+    </AnimationStyles>
 )
 
 export default CartCount
